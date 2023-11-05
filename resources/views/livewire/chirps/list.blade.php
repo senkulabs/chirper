23b4256fa4cb9f5cf418c90089c72b0b5af68a21
Kresna Satya <kresnasatya72@gmail.com>
2023-11-05 21:12:52 +0800

    use computed property for chirps

--- resources/views/livewire/chirps/list.blade.php
+++ resources/views/livewire/chirps/list.blade.php
@@ -2,42 +2,38 @@
 
 use App\Models\Chirp;
 
-use function Livewire\Volt\{on, state};
+use function Livewire\Volt\{computed, on, state};
 
-$getChirps = fn () => $this->chirps = Chirp::with('user')->latest()->get();
+$chirps = computed(fn () => $this->chirps = Chirp::with('user')->latest()->get());
 
 $disableEditing = function () {
     $this->editing = null;
-
-    $this->getChirps();
 };
 
-state(['chirps' => $getChirps, 'editing' => null]);
+state(['editing' => null]);
 
 on([
-    'chirp-created' => $getChirps,
+    'chirp-created' => function () {
+        unset($this->chirps);
+    },
     'chirp-updated' => $disableEditing,
     'chirp-edit-canceled' => $disableEditing,
 ]);
 
 $edit = function (Chirp $chirp) {
     $this->editing = $chirp;
-
-    $this->getChirps();
 };
 
 $delete = function (Chirp $chirp) {
     $this->authorize('delete', $chirp);
 
     $chirp->delete();
-
-    $this->getChirps();
 };
 
 ?>
 
 <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
-    @foreach ($chirps as $chirp)
+    @foreach ($this->chirps as $chirp)
     <div class="p-6 flex space-x-2" wire-key="{{ $chirp->id }}">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
