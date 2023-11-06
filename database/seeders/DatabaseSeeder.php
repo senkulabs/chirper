5b5ad1db7b5b3d369d0ea44f342539522a035ac9
Kresna Satya <kresnasatya72@gmail.com>
2023-11-07 06:43:04 +0800

    create chirp factory and database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Chirp;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +17,20 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()
+        ->has(Chirp::factory()->count(3), 'chirps')
+        ->create([
+            'name' => 'Kresna',
+            'email' => 'kresna@example.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        User::factory()
+        ->has(Chirp::factory()->count(3), 'chirps')
+        ->create([
+            'name' => 'Satya',
+            'email' => 'satya@example.com',
+            'password' => bcrypt('12345678'),
+        ]);
     }
 }
